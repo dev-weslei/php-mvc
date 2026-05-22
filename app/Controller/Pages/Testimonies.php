6db6a5d886edea3069544eb3586b4642fe9344af
Weslei Aurelio <devweslei@gmail.com>
2026-05-22 11:18:48 -0300

    Criação método (getTestimonyItens) responsável por rederizar os depoimentos na view

--- app/Controller/Pages/Testimonies.php
+++ app/Controller/Pages/Testimonies.php
@@ -8,9 +8,35 @@ use App\Model\Entity\Testimony;
 
 class Testimonies extends Template {
 
+    /**
+     * Método responsável por obter a renderização dos itens para a página
+     *
+     * @return string
+     */
+    private static function getTestimonyItens() {
+        $itens = '';
+
+        // RESULTADOS DA PÁGINA
+        // $results recebe a query que foi gerada para capturar todos os depoimentos
+        $results = Testimony::getTestimonies(null, 'id DESC');
+
+        // RENDERIZA O ITEM
+        while ($obTestimony = $results->fetchObject(Testimony::class)) {
+            $itens .= View::render('pages/testimonies/item', [
+                'nome'          => $obTestimony->nome, 
+                'mensagem'      => $obTestimony->mensagem,
+                'data_criacao'  => date('d/m/Y H:i:s', strtotime($obTestimony->data_criacao))
+            ]);
+        }
+        // RETORNA OS DEPOIMENTOS
+        return $itens;
+    }
+
     public static function getTestimonies() {
         // view da Home
-        $content = View::render('pages/testimonies', []);
+        $content = View::render('pages/testimonies', [
+            'itens' => self::getTestimonyItens(),
+        ]);
 
        // view do Template
        return parent::getTemplate('HOME > Weslei', $content);
