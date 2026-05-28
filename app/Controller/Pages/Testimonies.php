c382d47f5dd451af94b454ab59327dd9d0b5a292
Weslei Aurelio <devweslei@gmail.com>
2026-05-28 10:25:38 -0300

    criacao logica para exibir registros de acordo com a paginacao

--- app/Controller/Pages/Testimonies.php
+++ app/Controller/Pages/Testimonies.php
@@ -5,20 +5,29 @@ namespace App\Controller\Pages;
 use App\Utils\View;
 use App\Http\Request;
 use App\Model\Entity\Testimony;
+use WilliamCosta\DatabaseManager\Pagination;
 
 class Testimonies extends Template {
 
     /**
      * Método responsável por obter a renderização dos itens para a página
-     *
+     * @param Request $request
+     * @param Pagination $obPagination
      * @return string
      */
-    private static function getTestimonyItens() {
+    private static function getTestimonyItens($request, &$obPagination) {
         $itens = '';
 
-        // RESULTADOS DA PÁGINA
-        // $results recebe a query que foi gerada para capturar todos os depoimentos
-        $results = Testimony::getTestimonies(null, 'id DESC');
+        $quantidadeTotalDepoimentos = Testimony::getTestimonies(null, null, null, 'COUNT(*) as qtd')
+                                        ->fetchObject()
+                                        ->qtd;
+
+        $paginaAtual = $request->getQueryParams();
+        $paginaAtual = $paginaAtual['page'] ?? 1;
+
+        // instância de paginação
+        $obPagination = new Pagination($quantidadeTotalDepoimentos, $paginaAtual, 3);
+        $results = Testimony::getTestimonies(null, 'id DESC', $obPagination->getLimit());
 
         // RENDERIZA O ITEM
         while ($obTestimony = $results->fetchObject(Testimony::class)) {
@@ -28,14 +37,22 @@ class Testimonies extends Template {
                 'data_criacao'  => date('d/m/Y H:i:s', strtotime($obTestimony->data_criacao))
             ]);
         }
+
         // RETORNA OS DEPOIMENTOS
         return $itens;
     }
-
-    public static function getTestimonies() {
+    
+    /**
+     * Método responsável por renderizar a view de Depoimentos
+     *
+     * @param Request $request
+     * @return string
+     */
+    public static function getTestimonies($request) {
         // view da Home
         $content = View::render('pages/testimonies', [
-            'itens' => self::getTestimonyItens(),
+            'itens' => self::getTestimonyItens($request, $obPagination),
+            'pagination' => parent::getPagination($request, $obPagination),
         ]);
 
        // view do Template
@@ -56,6 +73,6 @@ class Testimonies extends Template {
         $obTestimony->mensagem = $postVars['mensagem'];
         $obTestimony->cadastrar();
 
-        return self::getTestimonies();
+        return self::getTestimonies($request);
     }
 }   
