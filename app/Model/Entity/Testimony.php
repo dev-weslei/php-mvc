fe10b0cea5baf0b38e2819105199edb94b57461f
Weslei Aurelio <devweslei@gmail.com>
2026-07-02 09:08:13 -0300

    criacao metodos atualizar e getTestimonyById Model Testimony

--- app/Model/Entity/Testimony.php
+++ app/Model/Entity/Testimony.php
@@ -8,28 +8,24 @@ class Testimony {
 
     /**
      * Indentificador unico do depoimento
-     *
      * @var integer
      */
     public $id;
 
     /**
      * Nome do criador do depoimento
-     *
      * @var string
      */
     public $nome;
 
     /**
      * Mensagem (depoimento) do usuário
-     *
      * @var string
      */
     public $mensagem;
 
     /**
      * Data e hora em que o depoimento foi criado
-     *
      * @var string (quando lê do db é uma string)
      */
     public $data_criacao;
@@ -37,14 +33,10 @@ class Testimony {
 
     /**
      * Método responsável por cadastrar a instância atual no banco de dados
-     *
      * @return boolean
      */
     public function cadastrar() {
-        // DEFINE A DATA
         $this->data_criacao = date('Y-m-d H:i:s');
-
-        // INSERE O DEPOIMENTO NO BANCO DE DADOS
         $this->id = (new Database('testimony'))->insert([
             'nome'         => $this->nome,
             'mensagem'     => $this->mensagem,
@@ -52,6 +44,26 @@ class Testimony {
         ]);
 
         return true;
+    }
+
+    /**
+     * Método responsável por atualizar as informações do depoimento no banco de dados
+     * @return boolean
+     */
+    public function atualizar() {
+        return (new Database('testimony'))->update('id ='.$this->id, [
+            'nome'         => $this->nome,
+            'mensagem'     => $this->mensagem,
+            'data_criacao' => $this->data_criacao
+        ]);
+    }
+
+    /**
+     * Método responsável por excluir um depoimento do banco de dados
+     * @return boolean
+     */
+    public function excluir() {
+        return (new Database('testimony'))->delete('id ='.$this->id);
     }
 
     /**
@@ -77,5 +89,14 @@ class Testimony {
             $limit, 
             $fields
         );
+    }
+
+    /**
+     * Método responsável por retornar um depoimento com base no seu ID
+     * @param integer $id
+     * @return Testimony
+     */
+    public static function getTestimonyById($id) {
+        return self::getTestimonies('id ='.$id)->fetchObject(self::class);
     }
 }
