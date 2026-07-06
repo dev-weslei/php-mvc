767e28c9790db02976a09b1bc47420e7d213bbc5
Weslei Aurelio <devweslei@gmail.com>
2026-07-06 09:20:33 -0300

    criacao métodos utilizados para buscar, editar e excluir usuários do banco de dados

--- app/Model/Entity/User.php
+++ app/Model/Entity/User.php
@@ -4,7 +4,8 @@ namespace App\Model\Entity;
 
 use WilliamCosta\DatabaseManager\Database;
 
-class User {
+class User
+{
     /**
      * Indentificador unico do usuário
      * @var integer
@@ -34,11 +35,84 @@ class User {
      * @param string $email
      * @return User
      */
-    public static function getUserByEmail($email) {
+    public static function getUserByEmail($email)
+    {
+        return self::getUsers('email = "' . $email . '"')
+            ->fetchObject(self::class);
+    }
+
+    /**
+     * Método responsável por retornar uma instância de User com base no id
+     * @param string $id
+     * @return User
+     */
+    public static function getUserById($id)
+    {
+        return self::getUsers('id = ' . $id)
+            ->fetchObject(self::class);
+    }
+
+    /**
+     * Método responsável por retornar todos os usuários cadastrados
+     * @param string $where
+     * @param string $order
+     * @param string $limit
+     * @param string $fields
+     * @return \PDOStatement
+     */
+    public static function getUsers(
+        $where = null,
+        $order = null,
+        $limit = null,
+        $fields = '*'
+    ) {
         return (
             new Database('users')
-            )
-        ->select('email = "'.$email.'"')
-        ->fetchObject(self::class);
+        )->select(
+            $where,
+            $order,
+            $limit,
+            $fields
+        );
+    }
+
+    /**
+     * Método responsável por cadastrar a instância atual no banco de dados
+     * @return boolean
+     */
+    public function cadastrar()
+    {
+        $this->id = (new Database('users'))->insert([
+            'nome'  => $this->nome,
+            'email' => $this->email,
+            'senha' => $this->senha
+        ]);
+
+        return true;
+    }
+
+    /**
+     * Método responsável por atualizar o objeto no banco de dados
+     * @return boolean
+     */
+    public function atualizar()
+    {
+        return (new Database('users'))->update(
+            'id = ' . $this->id,
+            [
+                'nome'  => $this->nome,
+                'email' => $this->email,
+                'senha' => $this->senha
+            ]
+        );
+    }
+
+    /**
+     * Método responsável por excluir o usuário do banco
+     * @return boolean
+     */
+    public function excluir()
+    {
+        return (new Database('users'))->delete('id = ' . $this->id);
     }
 }
