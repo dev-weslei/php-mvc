a52f4e336c8dd6efd1ffc9d3b69c3e0a239c2513
Weslei Aurelio <devweslei@gmail.com>
2026-07-07 08:52:11 -0300

    insercao include rotas api arquivo de entrada da aplicacao

--- index.php
+++ index.php
@@ -10,6 +10,9 @@ $obRouter = new Router(getenv('URL'));
 // INCLUI AS ROTAS
 include __DIR__.'/routes/pages.php';
 include __DIR__.'/routes/admin.php';
+
+// Inclui as rotas da API
+include __DIR__.'/routes/api.php';
  
 // IMPRIME O RESPONSE DA ROTA
 $obRouter->run()
