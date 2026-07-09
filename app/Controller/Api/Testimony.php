fa1fd430549ed87d9c42426a895d1b3496f80fdb
Weslei Aurelio <devweslei@gmail.com>
2026-07-09 10:54:55 -0300

    criacao Controller Testimony CRUD api

--- app/Controller/Api/Testimony.php
+++ app/Controller/Api/Testimony.php
@@ -13,7 +13,7 @@ class Testimony extends Api
      * Método responsável por obter a renderização dos itens para a página
      * @param Request $request
      * @param Pagination $obPagination
-     * @return string
+     * @return array
      */
     private static function getTestimonyItens($request, &$obPagination)
     {
@@ -65,7 +65,7 @@ class Testimony extends Api
     public static function getTestimony($id)
     {
         if (!is_numeric($id)) {
-            throw new Exception("O ID '".$id."' não é válido.", 400);
+            throw new Exception("O ID '" . $id . "' não é válido.", 400);
         }
 
         $obTestimony = TestimonyEntity::getTestimonyById($id);
@@ -78,6 +78,83 @@ class Testimony extends Api
             'nome'          => $obTestimony->nome,
             'texto'         => $obTestimony->mensagem,
             'data-criacao'  => $obTestimony->data_criacao
+        ];
+    }
+
+    /**
+     * Método responsável por cadastrar um novo depoimento
+     * @param Request $request
+     * @return void
+     */
+    public static function setNewTestimony($request)
+    {
+        $postVars = $request->getPostVars();
+        if (!isset($postVars['nome']) || !isset($postVars['mensagem'])) {
+            throw new Exception("Os campos 'nome' e 'mensagem' são obrigatórios!", 400);
+        }
+
+        $obTestimony           = new TestimonyEntity;
+        $obTestimony->nome     = $postVars['nome'];
+        $obTestimony->mensagem = $postVars['mensagem'];
+        $obTestimony->cadastrar();
+
+        // Retorna os detalhes do depoimento cadastrado
+        return [
+            'id'            => (int) $obTestimony->id,
+            'nome'          => $obTestimony->nome,
+            'texto'         => $obTestimony->mensagem,
+            'data-criacao'  => $obTestimony->data_criacao
+        ];
+    }
+
+    /**
+     * Método responsável por atualizar um depoimento
+     * @param Request $request
+     * @param integer $id
+     * @return array
+     */
+    public static function setEditTestimony($request, $id)
+    {
+        $postVars = $request->getPostVars();
+        if (!isset($postVars['nome']) || !isset($postVars['mensagem'])) {
+            throw new Exception("Os campos 'nome' e 'mensagem' são obrigatórios!", 400);
+        }
+
+        $obTestimony = TestimonyEntity::getTestimonyById($id);
+        if (!$obTestimony instanceof TestimonyEntity) {
+            throw new Exception("O depoimento com ID " . $id . " não foi encontrado.", 404);
+        }
+
+        $obTestimony->nome     = $postVars['nome'];
+        $obTestimony->mensagem = $postVars['mensagem'];
+        $obTestimony->atualizar();
+
+        // Retorna os detalhes do depoimento atualizado
+        return [
+            'id'           => (int) $obTestimony->id,
+            'nome'         => $obTestimony->nome,
+            'texto'        => $obTestimony->mensagem,
+            'data-criacao' => $obTestimony->data_criacao
+        ];
+    }
+
+    /**
+     * Método responsável por excluir um Depoimento do banco de dados
+     * @param Request $request
+     * @param integer $id
+     * @return void
+     */
+    public static function setDeleteTestimony($request, $id)
+    {
+        $obTestimony = TestimonyEntity::getTestimonyById($id);
+        if (!$obTestimony instanceof TestimonyEntity) {
+            throw new Exception("O depoimento com ID " . $id . " não foi encontrado.", 404);
+        }
+
+        $obTestimony->excluir();
+
+        return [
+            'sucesso' => 'Depoimento com ID ' . $id . ' excluído com sucesso!'
         ];
     }
 }
