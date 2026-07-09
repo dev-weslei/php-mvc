70638fdb909ef87882a33bb9304358e44699df1d
Weslei Aurelio <devweslei@gmail.com>
2026-07-09 10:53:24 -0300

    criacao atributo $user e tipo de dados postVars para api

--- app/Http/Request.php
+++ app/Http/Request.php
@@ -2,7 +2,10 @@
 
 namespace App\Http;
 
-class Request {
+use App\Model\Entity\User;
+
+class Request
+{
 
     /**
      * Instância de Router
@@ -47,49 +50,97 @@ class Request {
     private $headers = [];
 
     /**
+     * Classe de usuário
+     *
+     * @var User
+     */
+    private $user;
+
+    /**
      * Método responsável por inicializar a classe
      * @param Router $router
      */
-    public function __construct($router) {
+    public function __construct($router)
+    {
         $this->router      = $router;
         $this->queryParams = $_GET ?? [];
-        $this->postVars    = $_POST ?? [];
         $this->headers     = getallheaders();
         $this->httpMethod  = $_SERVER['REQUEST_METHOD'] ?? '';
         $this->setUri();
+        $this->setPostVars();
+    }
+
+    /**
+     * Método responsável por retornar a instância de User
+     * @return User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Método responsável por definir a instância de User
+     * @param User $obUser
+     */
+    public function setUser($obUser)
+    {
+        $this->user = $obUser;
+    }
+
+    /**
+     * Método responsável por definir as váriaveis do POST
+     */
+    private function setPostVars()
+    {
+        if ($this->httpMethod == 'GET') {
+            return false;
+        }
+
+        $this->postVars = $_POST ?? [];
+
+        // Valida o POST JSON
+        $inputRaw = file_get_contents('php://input');
+        $this->postVars = (strlen($inputRaw) && empty($_POST)) ? json_decode($inputRaw, true) : $this->postVars;
     }
 
     /**
      * Método responsável por definir a URI (sem queryParams)
      *
      */
-    private function setUri() {
+    private function setUri()
+    {
         $this->uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     }
 
     // Métodos acessores
-    public function getRouter() {
+    public function getRouter()
+    {
         return $this->router;
     }
 
-    public function getHttpMethod() {
+    public function getHttpMethod()
+    {
         return $this->httpMethod;
     }
 
-    public function getUri() {
+    public function getUri()
+    {
         return $this->uri;
     }
 
-    public function getQueryParams() {
+    public function getQueryParams()
+    {
         return $this->queryParams;
     }
 
-    public function getPostVars() {
+    public function getPostVars()
+    {
         return $this->postVars;
     }
-    
-    public function getHeaders() {
+
+    public function getHeaders()
+    {
         return $this->headers;
     }
-
 }
