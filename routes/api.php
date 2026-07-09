d8dffe5babcdc4e0b8f4c0e2ff250012894aa039
Weslei Aurelio <devweslei@gmail.com>
2026-07-09 10:50:12 -0300

    criacao include rotas usuarios api

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
 // Inclui rotas padrões da API
-include __DIR__.'/api/v1/default.php';
+include __DIR__ . '/api/v1/default.php';
 
 // Inclui rotas de depoimentos
-include __DIR__.'/api/v1/testimonies.php';
+include __DIR__ . '/api/v1/testimonies.php';
+
+// Inclui rotas de usuários
+include __DIR__ . '/api/v1/users.php';
