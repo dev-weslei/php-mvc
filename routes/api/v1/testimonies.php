2119789ffc41d01a93219fd16364520d58f3488a
Weslei Aurelio <devweslei@gmail.com>
2026-07-09 10:49:03 -0300

    criacao rotas api crud testimonies

--- routes/api/v1/testimonies.php
+++ routes/api/v1/testimonies.php
@@ -22,3 +22,37 @@ $obRouter->get('/api/v1/testimonies/{id}', [
         return new Response(200, Testimony::getTestimony($id), 'application/json');
     }
 ]);
+
+// Rota de cadastro de depoimentos
+$obRouter->post('/api/v1/testimonies', [
+    'middlewares' => [
+        'api',
+        'user-basic-auth'
+    ],
+    function ($request) {
+        // Retorno 201: Created
+        return new Response(201, Testimony::setNewTestimony($request), 'application/json');
+    }
+]);
+
+// Rota responsável por editar um depoimento
+$obRouter->put('/api/v1/testimonies/{id}', [
+    'middlewares' => [
+        'api',
+        'user-basic-auth'
+    ],
+    function ($request, $id) {
+        return new Response(200, Testimony::setEditTestimony($request, $id), 'application/json');
+    }
+]);
+
+// Rota responsável por excluír um depoimento
+$obRouter->delete('/api/v1/testimonies/{id}', [
+    'middlewares' => [
+        'api',
+        'user-basic-auth'
+    ],
+    function ($request, $id) {
+        return new Response(200, Testimony::setDeleteTestimony($request, $id), 'application/json');
+    }
+]);
