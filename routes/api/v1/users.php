27b2de6793ad6a232d73d8e8a07a89378da4cb69
Weslei Aurelio <devweslei@gmail.com>
2026-07-09 10:48:19 -0300

    criacao rotas api crud usuário

---
+++ routes/api/v1/users.php
@@ -0,0 +1,59 @@
+<?php
+
+use App\Controller\Api\User;
+use App\Http\Response;
+
+// Rota responsável por listar todos os usuários cadastrados no sistema
+$obRouter->get('/api/v1/users', [
+    'middlewares' => [
+        'api',
+        'user-basic-auth'
+    ],
+    function ($request) {
+        return new Response(200, User::getUsers($request), 'application/json');
+    }
+]);
+
+// Rota responsável por buscar usuário especifico
+$obRouter->get('/api/v1/users/{id}', [
+    'middlewares' => [
+        'api',
+        'user-basic-auth'
+    ],
+    function ($id) {
+        return new Response(200, User::getUserById($id), 'application/json');
+    }
+]);
+
+// Rota responsável por atualizar os dados de um usuário
+$obRouter->post('/api/v1/users/{id}', [
+    'middlewares' => [
+        'api',
+        'user-basic-auth'
+    ],
+    function ($request, $id) {
+        return new Response(200, User::setEditUser($request, $id), 'application/json');
+    }
+]);
+
+// Rota responsável por excluír um usuário
+$obRouter->delete('/api/v1/users/{id}', [
+    'middlewares' => [
+        'api',
+        'user-basic-auth'
+    ],
+    function ($request, $id) {
+        return new Response(200, User::setDeleteUser($request, $id), 'application/json');
+    }
+]);
+
+// Rota responsável por cadastrar novo usuário
+$obRouter->post('/api/v1/users', [
+    'middlewares' => [
+        'api',
+        'user-basic-auth'
+    ],
+    function ($request) {
+        return new Response(200, User::setNewUser($request), 'application/json');
+    }
+]);
